40edc860c53b73a077f3b0f74ce15b750e2eaa71
BrightHee <jjay3518@gmail.com>
2021-07-13 15:40:07 +0900

    기능 추가: 그림 도메인 추가
    그림 도메인을 추가했다.

---
+++ src/main/java/com/homepaintings/painting/Painting.java
@@ -0,0 +1,32 @@
+package com.homepaintings.painting;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of ="id")
+@Builder @NoArgsConstructor @AllArgsConstructor
+public class Painting {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private int price;
+
+    private int stock;
+
+    @Lob
+    private String image;
+
+    private String description;
+
+    private LocalDateTime createdDateTime;
+
+    private LocalDateTime updatedDateTime;
+
+}
